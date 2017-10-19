b0a30796468ead02ded40b25d4bdb68151c35374
Laboratório Mídiacom <gabcarrara@hotmail.com>
2017-10-19 18:03:41 -0200

    Busca do token ate a origem

--- src/main/java/Testes/BTCTest.java
+++ src/main/java/Testes/BTCTest.java
@@ -23,9 +23,11 @@ import org.bitcoinj.core.BlockChain;
 import org.bitcoinj.core.Coin;
 import org.bitcoinj.core.InsufficientMoneyException;
 import org.bitcoinj.core.NetworkParameters;
+import org.bitcoinj.core.PeerAddress;
 import org.bitcoinj.core.PeerGroup;
 import org.bitcoinj.core.Sha256Hash;
 import org.bitcoinj.core.Transaction;
+import org.bitcoinj.core.TransactionInput;
 import org.bitcoinj.core.TransactionOutput;
 import org.bitcoinj.params.RegTestParams;
 import org.bitcoinj.script.Script;
@@ -37,6 +39,7 @@ import org.bitcoinj.store.MemoryBlockStore;
 import org.bitcoinj.wallet.SendRequest;
 import org.bitcoinj.wallet.UnreadableWalletException;
 import org.bitcoinj.wallet.Wallet;
+import org.bitcoinj.wallet.listeners.WalletCoinsReceivedEventListener;
 import org.spongycastle.util.encoders.Hex;
 
 /**
@@ -68,71 +71,126 @@ public class BTCTest {
         PeerGroup peerGroup = new PeerGroup(networkParameters, blockChain);
         peerGroup.setUserAgent("Sample App", "0.14");
         peerGroup.addWallet(wallet);
-        wallet.addWatchedAddress(Address.fromBase58(networkParameters, "n4YthQUE2GSX2EUGx76832QDHq8drfNMYi"));
+        //wallet.addWatchedAddress(Address.fromBase58(networkParameters, "n4YthQUE2GSX2EUGx76832QDHq8drfNMYi"));
+
+        wallet.addCoinsReceivedEventListener(new WalletCoinsReceivedEventListener() {
+            public void onCoinsReceived(Wallet wallet, Transaction tx, Coin prevBalance, Coin newBalance) {
+                Transaction txParent = tx.getInputs().get(0).getConnectedOutput().getParentTransaction();
+                String tokenText = "";
+
+                System.out.println("");
+                System.out.println("*************");
+                System.out.println("Token recieved");
+
+                while (tokenText.equals("")) {
+
+                    txParent = txParent.getInputs().get(0).getConnectedOutput().getParentTransaction();
+                    if (txParent == null) {
+                        break;
+                    }
+                    String id = txParent.getHashAsString();
+                    tokenText = retrieveToken(id, wallet);
+                    System.out.println(tokenText);
+
+                }
+                System.out.println("*************");
+            }
+        });
+
         InetSocketAddress address = new InetSocketAddress("192.168.1.220", 18444);
+        PeerAddress add = new PeerAddress(address);
 
         peerGroup.setUseLocalhostPeerWhenPossible(false);
 
         peerGroup.start();
 
         //peerGroup.connectToLocalHost();
-        peerGroup.connectTo(address);
+        //peerGroup.connectTo(address);
+        peerGroup.addAddress(add);
+
         System.out.println("Started");
         peerGroup.downloadBlockChain();
+
         System.out.println("BlockChain downloaded");
 
         System.out.println(peerGroup.getConnectedPeers());
         System.out.println("Completo " + blockChain.getBestChainHeight());
         System.out.println("Send Address: " + wallet.currentReceiveAddress());
 
-        System.out.println(wallet.getWatchedOutputs(true));
-        System.out.println("Endereços assistidos: " + wallet.getWatchedAddresses());
+        //System.out.println(wallet.getWatchedOutputs(true));
+        //System.out.println("Endereços assistidos: " + wallet.getWatchedAddresses());
         System.out.println("Endereços: " + wallet.getIssuedReceiveAddresses());
         System.out.println("Moedas atuais " + wallet.getBalance(Wallet.BalanceType.AVAILABLE).toFriendlyString());
         // System.out.println(wallet.getTransactions(false).iterator().next());
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("Escolha sua opção:\n 1 - Enviar");
+
+        System.out.println("Escolha sua opção:\n 1 - Enviar\n 2 - Enviar Token\n 3 - Buscar Token\n 4 - Verificar ID");
         System.out.print("\\>");
 
         String sendAddress;
-        switch (sc.nextInt()) {
-            case 1:
-                System.out.println("Quanto quer enviar?");
-                System.out.print("\\>");
-                int qtd = sc.nextInt();
-                System.out.println("Qual endereço?");
-                System.out.print("\\>");
-                sendAddress = sc.next();
-                send(qtd, sendAddress, wallet, networkParameters);
-                break;
-            case 2:
-                System.out.println("O que quer enviar?");
-                System.out.print("\\>");
-                String token = sc.next();
-                System.out.println("Qual endereço?");
-                System.out.print("\\>");
-                sendAddress = sc.next();
-                sendToken(token, sendAddress, wallet, networkParameters);
-                break;
-            case 3:
-                System.out.println("Qual transação?");
-                System.out.print("\\>");
-                String txId = sc.next();
-                retrieveToken(txId, wallet);
+        boolean done = false;
+        while (!done) {
+            switch (sc.nextInt()) {
+                case 1:
+                    System.out.println("Quanto quer enviar?");
+                    System.out.print("\\>");
+                    int qtd = sc.nextInt();
+                    System.out.println("Qual endereço?");
+                    System.out.print("\\>");
+                    sendAddress = sc.next();
+                    send(qtd, sendAddress, wallet, networkParameters);
+                    break;
+                case 2:
+                    System.out.println("O que quer enviar?");
+                    System.out.print("\\>");
+                    String token = sc.next();
+                    System.out.println("Qual endereço?");
+                    System.out.print("\\>");
+                    sendAddress = sc.next();
+                    sendToken(token, sendAddress, wallet, networkParameters);
+                    break;
+                case 3:
+                    System.out.println("Qual transação?");
+                    System.out.print("\\>");
+                    String txId = sc.next();
+                    System.out.println(retrieveToken(txId, wallet));
+                    break;
+                case 4:
+                    String t = "";
+                    System.out.println("Qual transação?");
+                    System.out.print("\\>");
+                    String id = sc.next();
+                    Transaction tx = wallet.getTransaction(Sha256Hash.wrap(id));
+                    TransactionOutput out = tx.getInputs().get(0).getConnectedOutput();
 
-                break;
+                    if (out != null) {
+                        Transaction parentTx = out.getParentTransaction();
+                        System.out.println(parentTx.getHashAsString());
+                        t = retrieveToken(parentTx.getHashAsString(), wallet);
+                    }
 
-            default:
-                System.out.println("Sem opções, saindo do programa");
+                    if (t.equals("")) {
+                        System.out.println("Token not found!");
+                    } else {
+                        System.out.println(t);
+                    }
+
+                    break;
+                case 0:
+                    done = true;
+                    break;
+                default:
+                    System.out.println("Sem opções, tente novamente.");
+            }
         }
-
         try {
             wallet.saveToFile(f);
         } catch (IOException ex) {
             //Logger.getLogger(BTCTest.class.getName()).log(Level.SEVERE, null, ex);
             System.out.println("Carteira não pode ser salva.");
         }
+
         peerGroup.stop();
 
     }
@@ -169,8 +227,8 @@ public class BTCTest {
         Transaction tx = new Transaction(networkParameters);
         Script opReturn = new ScriptBuilder().op(ScriptOpCodes.OP_RETURN).data(token.getBytes()).build();
 
+        tx.addOutput(Coin.parseCoin("1"), Address.fromBase58(networkParameters, sendAddress));
         tx.addOutput(Coin.ZERO, opReturn);
-        tx.addOutput(Transaction.MIN_NONDUST_OUTPUT, Address.fromBase58(networkParameters, sendAddress));
 
         try {
             wallet.sendCoins(SendRequest.forTx(tx));
@@ -198,8 +256,8 @@ public class BTCTest {
 
     }
 
-    private static void retrieveToken(String txId, Wallet w) {
-
+    private static String retrieveToken(String txId, Wallet w) {
+        String resp = "";
         Sha256Hash id = Sha256Hash.wrap(txId);
         Transaction tx = w.getTransaction(id);
         String txS = tx.toString();
@@ -216,9 +274,11 @@ public class BTCTest {
                 token = token.replace("]", "");
                 //System.out.println(token);
 
-                String resp = new String(Hex.decode(token), StandardCharsets.UTF_8);
-                System.out.println(resp);
+                resp = new String(Hex.decode(token), StandardCharsets.UTF_8);
+                //System.out.println(resp);
+
             }
         }
+        return resp;
     }
 }
